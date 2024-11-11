5eef5b61e4a806e5a01eb584d3c3719952710642
daguecab <138701742+daguecab@users.noreply.github.com>
2024-11-11 18:33:22 +0100

    Corregir año en misc.js

--- js/misc.js
+++ js/misc.js
@@ -1,16 +1,17 @@
 function loadFooter() {
-    // Obtén el elemento donde deseas cargar el footer (por su id o clase)
-    const footerContainer = document.querySelector('.site-footer'); // Reemplaza 'site-footer' con el id o clase de tu contenedor de footer
+    // Obtén el elemento donde deseas cargar el footer
+    const footerContainer = document.querySelector('.site-footer'); // Cambia 'site-footer' por el id o clase de tu contenedor de footer
     if (footerContainer) {
         // Define el HTML del footer
         const footerHTML = `
         <div class="container">
           <div class="row">
               <div class="col-lg-3 col-10 text-center mb-4" style="margin:auto">
-                  <a class="navbar-brand" href="/">
+                  <a class="navbar-brand" href="index.html">
                       <img class="logo" style="margin-top: -5%;" src="images/Logo.png">
                   </a>
-                  <p class="copyright-text text-muted mt-lg-3 mb-4 mb-lg-0">Copyright © 2023 <strong>Yogures de Antaño</strong></p>
+                  <p class="copyright-text text-muted mt-lg-3 mb-4 mb-lg-0">Copyright © <span id="current-year"></span>
+                  <strong>Yogures de Antaño</strong></p>
                   <p class="copyright-text">Designed by <a href="https://www.letpro.es/" target="_blank">LetPro</a></p>
               </div>
 
@@ -19,43 +20,28 @@ function loadFooter() {
 
                   <ul class="footer-menu flex-wrap">
                       <li class="footer-menu-item"><a href="#" class="footer-menu-link">Quién soy</a></li>
-
                       <li class="footer-menu-item"><a href="#" class="footer-menu-link">Opiniones</a></li>
-
-                      <li class="footer-menu-item"><a href="productos-de-antaño" class="footer-menu-link">Productos</a></li>
-
+                      <li class="footer-menu-item"><a href="productos-de-antaño.html" class="footer-menu-link">Productos</a></li>
                       <li class="footer-menu-item"><a href="#" class="footer-menu-link">FAQs</a></li>
                   </ul>
               </div>
 
               <div class="col-lg-3 col-4">
-                  <!-- <h5 class="text-white mb-3">Social</h5>
-
-                  <ul class="social-icon">
-
-                      <li><a href="#" class="social-icon-link bi-youtube"></a></li>
-
-                      <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
-
-                      <li><a href="#" class="social-icon-link bi-instagram"></a></li>
-
-                      <li><a href="#" class="social-icon-link bi-skype"></a></li>
-                  </ul> -->
                   <h5 class="text-white mb-3">Aviso Legal</h5>
-
                   <ul class="footer-menu flex-wrap">
-                      <li class="footer-menu-item"><a href="terminos-y-condiciones" class="footer-menu-link">Términos y condiciones</a></li>
-
-                      <li class="footer-menu-item"><a href="politica-de-privacidad" class="footer-menu-link">Política de privacidad</a></li>
-
+                      <li class="footer-menu-item"><a href="terminos-y-condiciones.html" class="footer-menu-link">Términos y condiciones</a></li>
+                      <li class="footer-menu-item"><a href="politica-de-privacidad.html" class="footer-menu-link">Política de privacidad</a></li>
                       <li class="footer-menu-item"><a href="#" class="footer-menu-link">Política de ventas y envíos</a></li>
                   </ul>
               </div>
           </div>
         </div>
         `;
-
+        
         // Inserta el HTML del footer en el elemento contenedor
         footerContainer.innerHTML = footerHTML;
+        
+        // Ahora selecciona el elemento y establece el año actual
+        document.getElementById("current-year").textContent = new Date().getFullYear();
     }
 }

7dfba351e996be2467bfdc02ff719d68fa9d1496
daguecab <138701742+daguecab@users.noreply.github.com>
2024-12-11 18:23:59 +0100

    Incluir llamada a crm y extraer reviews zohoUtils.js

--- js/zohoUtils.js
+++ js/zohoUtils.js
@@ -1,3 +1,25 @@
+async function obtenerReviewsDesdeCrm(productCrmId) {
+    const url = `https://stripe-integration-n0er.onrender.com/zoho/productReviews?product=${productCrmId}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+
+        const reviews = await response.json();
+        return reviews; // Devuelve las reseñas
+    } catch (error) {
+        throw new Error(`Error al obtener las reseñas: ${error.message}`);
+    }
+}
+
 export function calcularNotaMedia(json) {
   const reviews = json.reviews;
   
@@ -17,3 +39,17 @@ export function calcularNotaMedia(json) {
   
   return promedio.toFixed(1); // Redondear a 2 decimales
 }
+
+export const extraerReviews = (reviews) => {
+  if (!Array.isArray(reviews)) {
+    throw new Error("El parámetro reviews debe ser un array");
+  }
+
+  // Mapeamos los datos necesarios de cada reseña
+  return reviews.map((review) => ({
+    nombre: review.Nombre_a_mostrar || "Anónimo",
+    comentario: review.Comentario || "Sin comentario",
+    calificacion: review.Calificaci_n || "Sin calificación",
+    fechaCreacion: review.Created_Time || "Fecha no disponible",
+  }));
+};
